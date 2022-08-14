ac69bab5ec02112b8b5fd3cb11a40f7930bd8c9d
Denys Romashkin <dr@adverty.com>
2022-08-14 16:25:33 +0300

    7kyu exercise

--- Codewars/7 kyu/odd_numbers_sum.py
+++ Codewars/7 kyu/odd_numbers_sum.py
@@ -12,10 +12,18 @@
 # 1 -->  1
 # 2 --> 3 + 5 = 8
 
+
+# bidlo's solution
 def row_sum_odd_numbers(n):
-    start_odd_number = sum(range(1, n))
-    return
+    start_odd_number = sum(range(n)) * 2 + 1
+    result_odd_number = start_odd_number
+    for i in range(n-1):
+        start_odd_number += 2
+        result_odd_number += start_odd_number
+    return result_odd_number
 
-number = 5
+print(row_sum_odd_numbers(5))
 
-print()
+# Human's solution
+def row_sum_odd_numbers_boss(n):
+    return n**3
